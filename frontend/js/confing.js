38a4b177279f07790a25e00f6e61b940d1f2c5f3
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-13 17:03:04 +0100

    Update confing.js

---
+++ frontend/js/confing.js
@@ -0,0 +1,37 @@
+window.CryptoZoo = window.CryptoZoo || {};
+
+window.CryptoZoo.config = {
+    animals: {
+        monkey: {
+            key: "monkey",
+            displayName: "Małpa",
+            buyName: "małpę",
+            buyCost: 100,
+            baseIncome: 1,
+            upgradeBaseCost: 150,
+            rarity: "Common",
+            rarityMultiplier: 1.0
+        },
+        panda: {
+            key: "panda",
+            displayName: "Panda",
+            buyName: "pandę",
+            buyCost: 400,
+            baseIncome: 3,
+            upgradeBaseCost: 600,
+            rarity: "Rare",
+            rarityMultiplier: 1.5
+        },
+        lion: {
+            key: "lion",
+            displayName: "Lew",
+            buyName: "lwa",
+            buyCost: 1200,
+            baseIncome: 8,
+            upgradeBaseCost: 1800,
+            rarity: "Epic",
+            rarityMultiplier: 2.5
+        }
+    },
+    maxOfflineSeconds: 86400
+};
