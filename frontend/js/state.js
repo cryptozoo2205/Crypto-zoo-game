220bc5b9812653a0bdc4032d70ad1e2b48b1c9b9
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-14 20:07:02 +0100

    Update state.js

--- frontend/js/state.js
+++ frontend/js/state.js
@@ -1,23 +1,21 @@
-
 window.CryptoZoo = window.CryptoZoo || {};
 
 window.CryptoZoo.state = {
-    coins: 0,
-    level: 1,
-    coinsPerClick: 1,
-    upgradeCost: 50,
-    zooIncome: 0,
 
-    animals: {
-        monkey: { count: 0, level: 1 },
-        panda: { count: 0, level: 1 },
-        lion: { count: 0, level: 1 }
-    },
+    coins:0,
 
-    telegramUser: null,
-    telegramId: "",
-    playerUsername: "",
+    level:1,
 
-    els: {},
-    loadingVisible: true
+    coinsPerClick:1,
+
+    upgradeCost:50,
+
+    zooIncome:0,
+
+    animals:{
+        monkey:{count:0,level:1},
+        panda:{count:0,level:1},
+        lion:{count:0,level:1}
+    }
+
 };
