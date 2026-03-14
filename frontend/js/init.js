2d59b9c4da8f3e2962a70d142dc2cfb4a87e7c5b
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-14 09:39:50 +0100

    Update init.js

--- frontend/js/init.js
+++ frontend/js/init.js
@@ -1,1 +1,28 @@
 
+window.CryptoZoo = window.CryptoZoo || {};
+
+window.CryptoZoo.init = {
+    async start() {
+        CryptoZoo.dom.cacheElements();
+        CryptoZoo.ui.showLoading();
+
+        CryptoZoo.telegram.setupPlayerIdentity();
+        CryptoZoo.gameplay.normalizeAnimals();
+        CryptoZoo.gameplay.updateZooIncome();
+        CryptoZoo.ui.render();
+
+        CryptoZoo.gameplay.bindNavigation();
+        CryptoZoo.gameplay.bindActions();
+
+        await CryptoZoo.gameplay.loadPlayerState();
+        await CryptoZoo.gameplay.loadRanking();
+
+        CryptoZoo.ui.showScreen("game");
+        CryptoZoo.gameplay.startPassiveIncome();
+        CryptoZoo.ui.hideLoading();
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    CryptoZoo.init.start();
+});
