b06c439afc5d9c9d81c04d5107f74a51309ef7c2
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-14 20:06:11 +0100

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,55 +1,55 @@
 window.CryptoZoo = window.CryptoZoo || {};
 
 window.CryptoZoo.config = {
+
+    clickUpgradeBaseCost: 50,
+
     animalConfig: {
+
         monkey: {
-            buyName: "małpę",
+            name: "Małpa",
             buyCost: 100,
             baseIncome: 1,
-            upgradeBaseCost: 150,
-            rarityMultiplier: 1.0
+            upgradeBaseCost: 150
         },
+
         panda: {
-            buyName: "pandę",
+            name: "Panda",
             buyCost: 400,
             baseIncome: 3,
-            upgradeBaseCost: 600,
-            rarityMultiplier: 1.5
+            upgradeBaseCost: 600
         },
+
         lion: {
-            buyName: "lwa",
+            name: "Lew",
             buyCost: 1200,
             baseIncome: 8,
-            upgradeBaseCost: 1800,
-            rarityMultiplier: 2.5
+            upgradeBaseCost: 1800
         }
+
     },
-    maxOfflineSeconds: 86400,
+
     passiveIncomeIntervalMs: 1000
 };
 
-window.CryptoZoo.formatNumber = function (num) {
-    if (num === null || num === undefined) return "0";
 
-    num = Number(num);
+window.CryptoZoo.formatNumber = function(num){
 
-    if (Number.isNaN(num)) return "0";
-
-    if (num >= 1000000000000) {
-        return (num / 1000000000000).toFixed(2) + "T";
+    if(num >= 1000000000000){
+        return (num/1000000000000).toFixed(2) + "T";
     }
 
-    if (num >= 1000000000) {
-        return (num / 1000000000).toFixed(2) + "B";
+    if(num >= 1000000000){
+        return (num/1000000000).toFixed(2) + "B";
     }
 
-    if (num >= 1000000) {
-        return (num / 1000000).toFixed(2) + "M";
+    if(num >= 1000000){
+        return (num/1000000).toFixed(2) + "M";
     }
 
-    if (num >= 1000) {
-        return (num / 1000).toFixed(2) + "K";
+    if(num >= 1000){
+        return (num/1000).toFixed(2) + "K";
     }
 
     return Math.floor(num).toString();
-};
+}
