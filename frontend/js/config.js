ed97b1bb707d03a76f87f00583d33e985d40e8fa
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-14 23:39:26 +0100

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,119 +1,114 @@
 window.CryptoZoo = window.CryptoZoo || {};
 
 window.CryptoZoo.config = {
-    economy: {
-        startCoins: 0,
-        startGems: 0,
-        startLevel: 1,
-        startCoinsPerClick: 1,
-        startUpgradeCost: 50,
-        clickUpgradeMultiplier: 2.4,
-        levelDivider: 5000,
-        passiveIncomeIntervalMs: 1000,
-        gemsPerLevel: 1
-    },
 
-    animals: {
-        monkey: {
-            key: "monkey",
-            name: "Małpa",
-            icon: "🐵",
-            rarity: "common",
-            buyCost: 100,
-            baseIncome: 1,
-            upgradeBaseCost: 150
-        },
-        rabbit: {
-            key: "rabbit",
-            name: "Królik",
-            icon: "🐰",
-            rarity: "common",
-            buyCost: 180,
-            baseIncome: 2,
-            upgradeBaseCost: 260
-        },
-        fox: {
-            key: "fox",
-            name: "Lis",
-            icon: "🦊",
-            rarity: "common",
-            buyCost: 320,
-            baseIncome: 4,
-            upgradeBaseCost: 420
-        },
-        wolf: {
-            key: "wolf",
-            name: "Wilk",
-            icon: "🐺",
-            rarity: "common",
-            buyCost: 550,
-            baseIncome: 6,
-            upgradeBaseCost: 700
-        },
-        panda: {
-            key: "panda",
-            name: "Panda",
-            icon: "🐼",
-            rarity: "rare",
-            buyCost: 900,
-            baseIncome: 10,
-            upgradeBaseCost: 1100
-        },
-        tiger: {
-            key: "tiger",
-            name: "Tygrys",
-            icon: "🐯",
-            rarity: "rare",
-            buyCost: 1500,
-            baseIncome: 16,
-            upgradeBaseCost: 1750
-        },
-        gorilla: {
-            key: "gorilla",
-            name: "Goryl",
-            icon: "🦍",
-            rarity: "rare",
-            buyCost: 2400,
-            baseIncome: 24,
-            upgradeBaseCost: 2600
-        },
-        lion: {
-            key: "lion",
-            name: "Lew",
-            icon: "🦁",
-            rarity: "epic",
-            buyCost: 4200,
-            baseIncome: 40,
-            upgradeBaseCost: 4500
-        },
-        elephant: {
-            key: "elephant",
-            name: "Słoń",
-            icon: "🐘",
-            rarity: "epic",
-            buyCost: 7600,
-            baseIncome: 70,
-            upgradeBaseCost: 8000
-        },
-        giraffe: {
-            key: "giraffe",
-            name: "Żyrafa",
-            icon: "🦒",
-            rarity: "epic",
-            buyCost: 12000,
-            baseIncome: 110,
-            upgradeBaseCost: 12600
-        }
-    }
+economy: {
+startCoins: 0,
+startGems: 0,
+startLevel: 1,
+startCoinsPerClick: 1,
+startUpgradeCost: 50,
+clickUpgradeMultiplier: 2.4,
+levelDivider: 5000,
+passiveIncomeIntervalMs: 1000,
+gemsPerLevel: 1
+},
+
+animals: {
+
+monkey:{
+key:"monkey",
+name:"Monkey",
+buyCost:100,
+baseIncome:1,
+upgradeBaseCost:150
+},
+
+rabbit:{
+key:"rabbit",
+name:"Rabbit",
+buyCost:300,
+baseIncome:2,
+upgradeBaseCost:400
+},
+
+fox:{
+key:"fox",
+name:"Fox",
+buyCost:800,
+baseIncome:4,
+upgradeBaseCost:1000
+},
+
+wolf:{
+key:"wolf",
+name:"Wolf",
+buyCost:2000,
+baseIncome:8,
+upgradeBaseCost:3000
+},
+
+panda:{
+key:"panda",
+name:"Panda",
+buyCost:6000,
+baseIncome:15,
+upgradeBaseCost:8000
+},
+
+tiger:{
+key:"tiger",
+name:"Tiger",
+buyCost:15000,
+baseIncome:30,
+upgradeBaseCost:20000
+},
+
+gorilla:{
+key:"gorilla",
+name:"Gorilla",
+buyCost:50000,
+baseIncome:70,
+upgradeBaseCost:70000
+},
+
+lion:{
+key:"lion",
+name:"Lion",
+buyCost:150000,
+baseIncome:150,
+upgradeBaseCost:200000
+},
+
+elephant:{
+key:"elephant",
+name:"Elephant",
+buyCost:500000,
+baseIncome:400,
+upgradeBaseCost:700000
+},
+
+giraffe:{
+key:"giraffe",
+name:"Giraffe",
+buyCost:1500000,
+baseIncome:900,
+upgradeBaseCost:2000000
+}
+
+}
+
 };
 
-window.CryptoZoo.formatNumber = function (num) {
-    num = Number(num) || 0;
+window.CryptoZoo.formatNumber=function(num){
 
-    if (num >= 1e12) return (num / 1e12).toFixed(2) + "T";
-    if (num >= 1e9) return (num / 1e9).toFixed(2) + "B";
-    if (num >= 1e6) return (num / 1e6).toFixed(2) + "M";
-    if (num >= 1e3) return (num / 1e3).toFixed(2) + "K";
+num=Number(num)||0;
 
-    return Math.floor(num).toString();
+if(num>=1e12)return(num/1e12).toFixed(2)+"T";
+if(num>=1e9)return(num/1e9).toFixed(2)+"B";
+if(num>=1e6)return(num/1e6).toFixed(2)+"M";
+if(num>=1e3)return(num/1e3).toFixed(2)+"K";
+
+return Math.floor(num).toString();
+
 };
