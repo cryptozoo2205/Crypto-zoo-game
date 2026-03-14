ff931844e9e8bd56b6487367b9490cc8cb256fa1
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-14 23:02:00 +0100

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -17,23 +17,92 @@ window.CryptoZoo.config = {
         monkey: {
             key: "monkey",
             name: "Małpa",
+            icon: "🐵",
+            rarity: "common",
             buyCost: 100,
             baseIncome: 1,
             upgradeBaseCost: 150
         },
+        rabbit: {
+            key: "rabbit",
+            name: "Królik",
+            icon: "🐰",
+            rarity: "common",
+            buyCost: 180,
+            baseIncome: 2,
+            upgradeBaseCost: 260
+        },
+        fox: {
+            key: "fox",
+            name: "Lis",
+            icon: "🦊",
+            rarity: "common",
+            buyCost: 320,
+            baseIncome: 4,
+            upgradeBaseCost: 420
+        },
+        wolf: {
+            key: "wolf",
+            name: "Wilk",
+            icon: "🐺",
+            rarity: "common",
+            buyCost: 550,
+            baseIncome: 6,
+            upgradeBaseCost: 700
+        },
         panda: {
             key: "panda",
             name: "Panda",
-            buyCost: 400,
-            baseIncome: 3,
-            upgradeBaseCost: 600
+            icon: "🐼",
+            rarity: "rare",
+            buyCost: 900,
+            baseIncome: 10,
+            upgradeBaseCost: 1100
+        },
+        tiger: {
+            key: "tiger",
+            name: "Tygrys",
+            icon: "🐯",
+            rarity: "rare",
+            buyCost: 1500,
+            baseIncome: 16,
+            upgradeBaseCost: 1750
+        },
+        gorilla: {
+            key: "gorilla",
+            name: "Goryl",
+            icon: "🦍",
+            rarity: "rare",
+            buyCost: 2400,
+            baseIncome: 24,
+            upgradeBaseCost: 2600
         },
         lion: {
             key: "lion",
             name: "Lew",
-            buyCost: 1200,
-            baseIncome: 8,
-            upgradeBaseCost: 1800
+            icon: "🦁",
+            rarity: "epic",
+            buyCost: 4200,
+            baseIncome: 40,
+            upgradeBaseCost: 4500
+        },
+        elephant: {
+            key: "elephant",
+            name: "Słoń",
+            icon: "🐘",
+            rarity: "epic",
+            buyCost: 7600,
+            baseIncome: 70,
+            upgradeBaseCost: 8000
+        },
+        giraffe: {
+            key: "giraffe",
+            name: "Żyrafa",
+            icon: "🦒",
+            rarity: "epic",
+            buyCost: 12000,
+            baseIncome: 110,
+            upgradeBaseCost: 12600
         }
     }
 };
