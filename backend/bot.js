33ec4f81547b072ae71778215a2610058369ed75
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-14 08:40:21 +0100

    Update bot.js

--- backend/bot.js
+++ backend/bot.js
@@ -1,26 +1,27 @@
-const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config()
 
-const TOKEN = "8723087229:AAGi_OCgJur6VSmGWatGdB1mk_UUR6PRvnk";
+const TelegramBot = require("node-telegram-bot-api")
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+const token = process.env.BOT_TOKEN
+const WEBAPP_URL = process.env.WEBAPP_URL
+
+const bot = new TelegramBot(token, { polling: true })
 
 bot.onText(/\/start/, (msg) => {
 
-    const chatId = msg.chat.id;
+    const chatId = msg.chat.id
 
-    bot.sendMessage(chatId, "🐾 Crypto Zoo", {
+    bot.sendMessage(chatId, "🐾 Witaj w Crypto Zoo!", {
         reply_markup: {
             inline_keyboard: [
                 [
                     {
-                        text: "🎮 Otwórz grę",
-                        web_app: {
-                            url: "https://tobi-nonextenuative-alycia.ngrok-free.dev"
-                        }
+                        text: "🎮 Zagraj w Crypto Zoo",
+                        web_app: { url: WEBAPP_URL }
                     }
                 ]
             ]
         }
-    });
+    })
 
-});
+})
