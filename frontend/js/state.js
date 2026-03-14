72a3ff1959033928d30356d8cd4fc73c7dcd9898
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-14 22:38:08 +0100

    Update state.js

--- frontend/js/state.js
+++ frontend/js/state.js
@@ -1,4 +1,4 @@
-window.CryptoZoo = window.CryptoZoo || {};
+lwindow.CryptoZoo = window.CryptoZoo || {};
 
 window.CryptoZoo.state = {
     coins: 0,
@@ -9,14 +9,7 @@ window.CryptoZoo.state = {
     zooIncome: 0,
     animals: {
         monkey: { count: 0, level: 1 },
-        rabbit: { count: 0, level: 1 },
-        parrot: { count: 0, level: 1 },
-        turtle: { count: 0, level: 1 },
         panda: { count: 0, level: 1 },
-        wolf: { count: 0, level: 1 },
-        flamingo: { count: 0, level: 1 },
-        lion: { count: 0, level: 1 },
-        tiger: { count: 0, level: 1 },
-        elephant: { count: 0, level: 1 }
+        lion: { count: 0, level: 1 }
     }
 };
