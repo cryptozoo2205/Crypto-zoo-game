710e8616c4088300511faaf7f2fd3070316e948e
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-16 21:30:13 +0100

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,111 +1,61 @@
 export const GAME_CONFIG = {
 
-  clickValue: 1,
+clickValue: 1,
 
-  offlineLimitHours: 8,
+offlineLimitHours: 8,
 
-  animals: [
+animals: [
 
-    {
-      id: "monkey",
-      name: "Monkey",
-      price: 100,
-      income: 1
-    },
+{ id:"monkey", name:"Monkey", price:100, income:1 },
+{ id:"panda", name:"Panda", price:400, income:3 },
+{ id:"lion", name:"Lion", price:1200, income:8 },
+{ id:"tiger", name:"Tiger", price:3500, income:20 },
+{ id:"elephant", name:"Elephant", price:9000, income:45 },
+{ id:"giraffe", name:"Giraffe", price:25000, income:100 },
+{ id:"zebra", name:"Zebra", price:70000, income:250 },
+{ id:"hippo", name:"Hippo", price:180000, income:600 },
+{ id:"penguin", name:"Penguin", price:500000, income:1500 },
+{ id:"bear", name:"Bear", price:1200000, income:3500 },
+{ id:"crocodile", name:"Crocodile", price:3000000, income:8000 },
+{ id:"kangaroo", name:"Kangaroo", price:7500000, income:18000 },
+{ id:"wolf", name:"Wolf", price:18000000, income:40000 }
 
-    {
-      id: "panda",
-      name: "Panda",
-      price: 400,
-      income: 3
-    },
+],
 
-    {
-      id: "lion",
-      name: "Lion",
-      price: 1200,
-      income: 8
-    },
+shopItems: [
 
-    {
-      id: "tiger",
-      name: "Tiger",
-      price: 3500,
-      income: 20
-    },
+{
+id:"click_upgrade",
+name:"Click Upgrade",
+desc:"+1 click power",
+price:200,
+type:"click"
+},
 
-    {
-      id: "elephant",
-      name: "Elephant",
-      price: 9000,
-      income: 45
-    },
+{
+id:"zoo_manager",
+name:"Zoo Manager",
+desc:"+25% zoo income",
+price:5000,
+type:"income"
+},
 
-    {
-      id: "giraffe",
-      name: "Giraffe",
-      price: 25000,
-      income: 100
-    },
+{
+id:"expedition_boost",
+name:"Expedition Gear",
+desc:"+20% expedition rewards",
+price:15000,
+type:"expedition"
+},
 
-    {
-      id: "zebra",
-      name: "Zebra",
-      price: 70000,
-      income: 250
-    },
+{
+id:"offline_boost",
+name:"Offline Booster",
+desc:"x2 offline income",
+price:25000,
+type:"offline"
+}
 
-    {
-      id: "hippo",
-      name: "Hippo",
-      price: 180000,
-      income: 600
-    },
+]
 
-    {
-      id: "penguin",
-      name: "Penguin",
-      price: 500000,
-      income: 1500
-    },
-
-    {
-      id: "bear",
-      name: "Bear",
-      price: 1200000,
-      income: 3500
-    },
-
-    {
-      id: "crocodile",
-      name: "Crocodile",
-      price: 3000000,
-      income: 8000
-    },
-
-    {
-      id: "kangaroo",
-      name: "Kangaroo",
-      price: 7500000,
-      income: 18000
-    },
-
-    {
-      id: "wolf",
-      name: "Wolf",
-      price: 18000000,
-      income: 40000
-    }
-
-  ],
-
-  upgrade: {
-
-    click: {
-      baseCost: 200,
-      multiplier: 1.4
-    }
-
-  }
-
-};
+}
