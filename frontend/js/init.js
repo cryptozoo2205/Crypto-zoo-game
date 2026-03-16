b51d69dc67a0e013e4456b4289e8ff907c02dbf8
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-16 22:17:17 +0100

    Update init.js

--- frontend/js/init.js
+++ frontend/js/init.js
@@ -7,55 +7,53 @@ console.log("Crypto Zoo start");
 try {
 
 /* TELEGRAM INIT */
-
 if (CryptoZoo.telegram && CryptoZoo.telegram.init) {
 CryptoZoo.telegram.init();
 }
 
 /* LOAD PLAYER DATA */
-
 if (CryptoZoo.api && CryptoZoo.api.loadPlayer) {
 await CryptoZoo.api.loadPlayer();
 }
 
 /* RENDER UI */
-
 if (CryptoZoo.ui && CryptoZoo.ui.render) {
 CryptoZoo.ui.render();
 }
 
 /* SHOP INIT */
-
 if (CryptoZoo.shop && CryptoZoo.shop.init) {
 CryptoZoo.shop.init();
 }
 
 /* BOX SYSTEM INIT */
-
 if (CryptoZoo.boxes && CryptoZoo.boxes.init) {
 CryptoZoo.boxes.init();
 }
 
-/* GAMEPLAY INIT */
+/* MINI GAMES INIT */
+if (CryptoZoo.minigames && CryptoZoo.minigames.init) {
+CryptoZoo.minigames.init();
+}
 
+/* DAILY REWARD INIT */
+if (CryptoZoo.daily && CryptoZoo.daily.init) {
+CryptoZoo.daily.init();
+}
+
+/* GAMEPLAY INIT */
 if (CryptoZoo.gameplay && CryptoZoo.gameplay.init) {
 CryptoZoo.gameplay.init();
 }
 
 } catch (error) {
-
 console.error("Init error:", error);
-
 }
 
 };
 
-
-
 document.addEventListener("DOMContentLoaded", function () {
-
 if (CryptoZoo.init) {
 CryptoZoo.init();
 }
-
 });
