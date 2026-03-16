e89fb60a6c56c31f99ce6f1d329954686b68c83f
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-16 13:59:27 +0100

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,79 +1,133 @@
 window.CryptoZoo = window.CryptoZoo || {};
 
-CryptoZoo.config = {
+window.CryptoZoo.config = {
+    economy: {
+        startCoins: 0,
+        startGems: 0,
+        startLevel: 1,
+        startCoinsPerClick: 1,
+        startUpgradeCost: 50,
+        clickUpgradeMultiplier: 2.2,
+        levelDivider: 20000,
+        passiveIncomeIntervalMs: 1000,
+        gemsPerLevel: 1
+    },
 
-animals:{
+    animals: {
+        monkey: {
+            key: "monkey",
+            name: "Małpa",
+            asset: "monkey",
+            buyCost: 100,
+            baseIncome: 1,
+            upgradeBaseCost: 150
+        },
+        panda: {
+            key: "panda",
+            name: "Panda",
+            asset: "panda",
+            buyCost: 400,
+            baseIncome: 3,
+            upgradeBaseCost: 600
+        },
+        lion: {
+            key: "lion",
+            name: "Lew",
+            asset: "lion",
+            buyCost: 1200,
+            baseIncome: 8,
+            upgradeBaseCost: 1800
+        },
+        tiger: {
+            key: "tiger",
+            name: "Tygrys",
+            asset: "tiger",
+            buyCost: 3000,
+            baseIncome: 15,
+            upgradeBaseCost: 4200
+        },
+        elephant: {
+            key: "elephant",
+            name: "Słoń",
+            asset: "elephant",
+            buyCost: 8000,
+            baseIncome: 35,
+            upgradeBaseCost: 11000
+        },
+        giraffe: {
+            key: "giraffe",
+            name: "Żyrafa",
+            asset: "giraffe",
+            buyCost: 18000,
+            baseIncome: 70,
+            upgradeBaseCost: 24000
+        },
+        zebra: {
+            key: "zebra",
+            name: "Zebra",
+            asset: "zebra",
+            buyCost: 35000,
+            baseIncome: 130,
+            upgradeBaseCost: 48000
+        },
+        penguin: {
+            key: "penguin",
+            name: "Pingwin",
+            asset: "penguin",
+            buyCost: 70000,
+            baseIncome: 240,
+            upgradeBaseCost: 95000
+        },
+        bear: {
+            key: "bear",
+            name: "Niedźwiedź",
+            asset: "bear",
+            buyCost: 140000,
+            baseIncome: 420,
+            upgradeBaseCost: 190000
+        },
+        wolf: {
+            key: "wolf",
+            name: "Wilk",
+            asset: "wolf",
+            buyCost: 280000,
+            baseIncome: 750,
+            upgradeBaseCost: 380000
+        }
+    },
 
-monkey:{
-name:"Małpa",
-asset:"monkey",
-baseIncome:1,
-buyCost:100
-},
+    expeditions: [
+        {
+            id: "short",
+            name: "Krótka wyprawa",
+            duration: 600,
+            rewardCoins: 500,
+            rewardGems: 1
+        },
+        {
+            id: "medium",
+            name: "Średnia wyprawa",
+            duration: 1800,
+            rewardCoins: 2000,
+            rewardGems: 2
+        },
+        {
+            id: "long",
+            name: "Długa wyprawa",
+            duration: 3600,
+            rewardCoins: 8000,
+            rewardGems: 5
+        }
+    ]
+};
 
-panda:{
-name:"Panda",
-asset:"panda",
-baseIncome:3,
-buyCost:400
-},
+window.CryptoZoo.formatNumber = function (num) {
+    num = Number(num) || 0;
 
-lion:{
-name:"Lew",
-asset:"lion",
-baseIncome:8,
-buyCost:1200
-},
+    if (num >= 1e12) return (num / 1e12).toFixed(2) + "T";
+    if (num >= 1e9) return (num / 1e9).toFixed(2) + "B";
+    if (num >= 1e6) return (num / 1e6).toFixed(2) + "M";
+    if (num >= 1e3) return (num / 1e3).toFixed(2) + "K";
 
-tiger:{
-name:"Tygrys",
-asset:"tiger",
-baseIncome:20,
-buyCost:5000
-},
-
-elephant:{
-name:"Słoń",
-asset:"elephant",
-baseIncome:50,
-buyCost:20000
-},
-
-giraffe:{
-name:"Żyrafa",
-asset:"giraffe",
-baseIncome:120,
-buyCost:60000
-}
-
-},
-
-expeditions:[
-
-{
-id:"short",
-name:"Krótka wyprawa",
-duration:600,
-rewardCoins:500,
-rewardGems:1
-},
-
-{
-id:"medium",
-name:"Średnia wyprawa",
-duration:1800,
-rewardCoins:2000,
-rewardGems:2
-},
-
-{
-id:"long",
-name:"Długa wyprawa",
-duration:3600,
-rewardCoins:8000,
-rewardGems:5
-}
-
-]
-
+    return Math.floor(num).toString();
 };
