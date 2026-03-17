f86fcb5631351b9fd0dfa1d3d9833a76107764a3
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-17 16:15:55 +0100

    Update init.js

--- frontend/js/init.js
+++ frontend/js/init.js
@@ -1,59 +1,35 @@
 window.CryptoZoo = window.CryptoZoo || {};
 
 CryptoZoo.init = async function () {
+    console.log("Crypto Zoo start");
 
-console.log("Crypto Zoo start");
+    try {
+        if (CryptoZoo.telegram && CryptoZoo.telegram.init) {
+            CryptoZoo.telegram.init();
+        }
 
-try {
+        if (CryptoZoo.api && CryptoZoo.api.loadPlayer) {
+            await CryptoZoo.api.loadPlayer();
+        }
 
-/* TELEGRAM INIT */
-if (CryptoZoo.telegram && CryptoZoo.telegram.init) {
-CryptoZoo.telegram.init();
-}
+        if (CryptoZoo.ui && CryptoZoo.ui.render) {
+            CryptoZoo.ui.render();
+        }
 
-/* LOAD PLAYER DATA */
-if (CryptoZoo.api && CryptoZoo.api.loadPlayer) {
-await CryptoZoo.api.loadPlayer();
-}
+        if (CryptoZoo.boxes && CryptoZoo.boxes.init) {
+            CryptoZoo.boxes.init();
+        }
 
-/* RENDER UI */
-if (CryptoZoo.ui && CryptoZoo.ui.render) {
-CryptoZoo.ui.render();
-}
-
-/* SHOP INIT */
-if (CryptoZoo.shop && CryptoZoo.shop.init) {
-CryptoZoo.shop.init();
-}
-
-/* BOX SYSTEM INIT */
-if (CryptoZoo.boxes && CryptoZoo.boxes.init) {
-CryptoZoo.boxes.init();
-}
-
-/* MINI GAMES INIT */
-if (CryptoZoo.minigames && CryptoZoo.minigames.init) {
-CryptoZoo.minigames.init();
-}
-
-/* DAILY REWARD INIT */
-if (CryptoZoo.daily && CryptoZoo.daily.init) {
-CryptoZoo.daily.init();
-}
-
-/* GAMEPLAY INIT */
-if (CryptoZoo.gameplay && CryptoZoo.gameplay.init) {
-CryptoZoo.gameplay.init();
-}
-
-} catch (error) {
-console.error("Init error:", error);
-}
-
+        if (CryptoZoo.gameplay && CryptoZoo.gameplay.init) {
+            CryptoZoo.gameplay.init();
+        }
+    } catch (error) {
+        console.error("Init error:", error);
+    }
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-if (CryptoZoo.init) {
-CryptoZoo.init();
-}
+    if (CryptoZoo.init) {
+        CryptoZoo.init();
+    }
 });
