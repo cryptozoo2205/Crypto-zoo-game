1a29581c73e133a9b99967a31c8af6e74a395352
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-03-17 17:38:31 +0100

    Update shop.js

--- frontend/js/shop.js
+++ frontend/js/shop.js
@@ -2,6 +2,17 @@ window.CryptoZoo = window.CryptoZoo || {};
 
 CryptoZoo.shop = {
     init() {
-        return true;
+        this.bindBoxBuyButtons();
+    },
+
+    bindBoxBuyButtons() {
+        const buttons = document.querySelectorAll("[data-box-type]");
+
+        buttons.forEach((button) => {
+            button.onclick = () => {
+                const type = button.getAttribute("data-box-type");
+                CryptoZoo.boxes?.buy?.(type);
+            };
+        });
     }
 };
