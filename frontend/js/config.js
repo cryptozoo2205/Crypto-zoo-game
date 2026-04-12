ce637fa87bd49c67fe76f30d65f39a6a1da9700e
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-04-12 20:58:35 +0200

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -6,38 +6,142 @@ CryptoZoo.config = {
     rewardToUsdRate: 0.05,
 
     limits: {
-        maxOwnedPerAnimal: 50,
-        maxLevelPerAnimal: 100
+        maxOwnedPerAnimal: 20,
+        maxLevelPerAnimal: 25
     },
 
     clickValue: 1,
 
     animals: {
-        monkey: { name: "Monkey", namePl: "Małpa", nameEn: "Monkey", asset: "monkey", buyCost: 120, baseIncome: 1 },
-        panda: { name: "Panda", namePl: "Panda", nameEn: "Panda", asset: "panda", buyCost: 520, baseIncome: 5 },
-        lion: { name: "Lion", namePl: "Lew", nameEn: "Lion", asset: "lion", buyCost: 2200, baseIncome: 14 },
-        tiger: { name: "Tiger", namePl: "Tygrys", nameEn: "Tiger", asset: "tiger", buyCost: 9000, baseIncome: 36 },
-        elephant: { name: "Elephant", namePl: "Słoń", nameEn: "Elephant", asset: "elephant", buyCost: 36000, baseIncome: 90 },
-        giraffe: { name: "Giraffe", namePl: "Żyrafa", nameEn: "Giraffe", asset: "giraffe", buyCost: 145000, baseIncome: 220 },
-        zebra: { name: "Zebra", namePl: "Zebra", nameEn: "Zebra", asset: "zebra", buyCost: 580000, baseIncome: 520 },
-        hippo: { name: "Hippo", namePl: "Hipopotam", nameEn: "Hippo", asset: "hippo", buyCost: 2300000, baseIncome: 1200 },
-        penguin: { name: "Penguin", namePl: "Pingwin", nameEn: "Penguin", asset: "penguin", buyCost: 9200000, baseIncome: 2700 },
-        bear: { name: "Bear", namePl: "Niedźwiedź", nameEn: "Bear", asset: "bear", buyCost: 37000000, baseIncome: 6000 },
-        crocodile: { name: "Crocodile", namePl: "Krokodyl", nameEn: "Crocodile", asset: "crocodile", buyCost: 148000000, baseIncome: 13200 },
-        kangaroo: { name: "Kangaroo", namePl: "Kangur", nameEn: "Kangaroo", asset: "kangaroo", buyCost: 590000000, baseIncome: 29000 },
-        wolf: { name: "Wolf", namePl: "Wilk", nameEn: "Wolf", asset: "wolf", buyCost: 2360000000, baseIncome: 64000 }
+        monkey: {
+            name: "Monkey",
+            namePl: "Małpa",
+            nameEn: "Monkey",
+            asset: "monkey",
+            buyCost: 150,
+            baseIncome: 1,
+            unlockLevel: 1
+        },
+        panda: {
+            name: "Panda",
+            namePl: "Panda",
+            nameEn: "Panda",
+            asset: "panda",
+            buyCost: 850,
+            baseIncome: 4,
+            unlockLevel: 3
+        },
+        lion: {
+            name: "Lion",
+            namePl: "Lew",
+            nameEn: "Lion",
+            asset: "lion",
+            buyCost: 4500,
+            baseIncome: 11,
+            unlockLevel: 6
+        },
+        tiger: {
+            name: "Tiger",
+            namePl: "Tygrys",
+            nameEn: "Tiger",
+            asset: "tiger",
+            buyCost: 18000,
+            baseIncome: 26,
+            unlockLevel: 10
+        },
+        elephant: {
+            name: "Elephant",
+            namePl: "Słoń",
+            nameEn: "Elephant",
+            asset: "elephant",
+            buyCost: 78000,
+            baseIncome: 58,
+            unlockLevel: 14
+        },
+        giraffe: {
+            name: "Giraffe",
+            namePl: "Żyrafa",
+            nameEn: "Giraffe",
+            asset: "giraffe",
+            buyCost: 340000,
+            baseIncome: 125,
+            unlockLevel: 18
+        },
+        zebra: {
+            name: "Zebra",
+            namePl: "Zebra",
+            nameEn: "Zebra",
+            asset: "zebra",
+            buyCost: 1450000,
+            baseIncome: 265,
+            unlockLevel: 22
+        },
+        hippo: {
+            name: "Hippo",
+            namePl: "Hipopotam",
+            nameEn: "Hippo",
+            asset: "hippo",
+            buyCost: 6200000,
+            baseIncome: 560,
+            unlockLevel: 27
+        },
+        penguin: {
+            name: "Penguin",
+            namePl: "Pingwin",
+            nameEn: "Penguin",
+            asset: "penguin",
+            buyCost: 27000000,
+            baseIncome: 1180,
+            unlockLevel: 32
+        },
+        bear: {
+            name: "Bear",
+            namePl: "Niedźwiedź",
+            nameEn: "Bear",
+            asset: "bear",
+            buyCost: 120000000,
+            baseIncome: 2450,
+            unlockLevel: 38
+        },
+        crocodile: {
+            name: "Crocodile",
+            namePl: "Krokodyl",
+            nameEn: "Crocodile",
+            asset: "crocodile",
+            buyCost: 540000000,
+            baseIncome: 5100,
+            unlockLevel: 44
+        },
+        kangaroo: {
+            name: "Kangaroo",
+            namePl: "Kangur",
+            nameEn: "Kangaroo",
+            asset: "kangaroo",
+            buyCost: 2400000000,
+            baseIncome: 10400,
+            unlockLevel: 50
+        },
+        wolf: {
+            name: "Wolf",
+            namePl: "Wilk",
+            nameEn: "Wolf",
+            asset: "wolf",
+            buyCost: 10500000000,
+            baseIncome: 21000,
+            unlockLevel: 58
+        }
     },
 
     expeditions: [
-        { id: "forest", name: "Magic Forest", namePl: "Magiczny Las", nameEn: "Magic Forest", duration: 300, baseDuration: 300, baseCoins: 80, startCostCoins: 50, baseGems: 0, gemChance: 0.01, rareChance: 0.12, epicChance: 0.01, unlockLevel: 1 },
-        { id: "river", name: "Crystal River", namePl: "Kryształowa Rzeka", nameEn: "Crystal River", duration: 900, baseDuration: 900, baseCoins: 350, startCostCoins: 260, baseGems: 0, gemChance: 0.016, rareChance: 0.13, epicChance: 0.015, unlockLevel: 3 },
-        { id: "volcano", name: "Golden Volcano", namePl: "Złoty Wulkan", nameEn: "Golden Volcano", duration: 1800, baseDuration: 1800, baseCoins: 1050, startCostCoins: 780, baseGems: 0, gemChance: 0.022, rareChance: 0.145, epicChance: 0.022, unlockLevel: 5 },
-        { id: "canyon", name: "Sunstone Canyon", namePl: "Kanion Słonecznego Kamienia", nameEn: "Sunstone Canyon", duration: 3600, baseDuration: 3600, baseCoins: 2900, startCostCoins: 2100, baseGems: 0, gemChance: 0.028, rareChance: 0.155, epicChance: 0.028, unlockLevel: 7 },
-        { id: "glacier", name: "Frozen Glacier", namePl: "Zamarznięty Lodowiec", nameEn: "Frozen Glacier", duration: 7200, baseDuration: 7200, baseCoins: 7600, startCostCoins: 5200, baseGems: 0, gemChance: 0.035, rareChance: 0.165, epicChance: 0.035, unlockLevel: 10 },
-        { id: "jungle", name: "Emerald Jungle", namePl: "Szmaragdowa Dżungla", nameEn: "Emerald Jungle", duration: 14400, baseDuration: 14400, baseCoins: 19000, startCostCoins: 12800, baseGems: 0, gemChance: 0.045, rareChance: 0.175, epicChance: 0.045, unlockLevel: 14 },
-        { id: "temple", name: "Ancient Temple", namePl: "Starożytna Świątynia", nameEn: "Ancient Temple", duration: 28800, baseDuration: 28800, baseCoins: 47000, startCostCoins: 29500, baseGems: 0, gemChance: 0.058, rareChance: 0.185, epicChance: 0.058, unlockLevel: 18 },
-        { id: "oasis", name: "Royal Oasis", namePl: "Królewska Oaza", nameEn: "Royal Oasis", duration: 43200, baseDuration: 43200, baseCoins: 104000, startCostCoins: 63000, baseGems: 0, gemChance: 0.072, rareChance: 0.195, epicChance: 0.072, unlockLevel: 22 },
-        { id: "kingdom", name: "Lost Beast Kingdom", namePl: "Zaginione Królestwo Bestii", nameEn: "Lost Beast Kingdom", duration: 86400, baseDuration: 86400, baseCoins: 235000, startCostCoins: 132000, baseGems: 0, gemChance: 0.09, rareChance: 0.21, epicChance: 0.09, unlockLevel: 28 }
+        { id: "forest", name: "Magic Forest", namePl: "Magiczny Las", nameEn: "Magic Forest", duration: 300, baseDuration: 300, baseCoins: 70, startCostCoins: 55, baseGems: 0, gemChance: 0.01, rareChance: 0.12, epicChance: 0.01, unlockLevel: 1 },
+        { id: "river", name: "Crystal River", namePl: "Kryształowa Rzeka", nameEn: "Crystal River", duration: 900, baseDuration: 900, baseCoins: 260, startCostCoins: 220, baseGems: 0, gemChance: 0.014, rareChance: 0.13, epicChance: 0.014, unlockLevel: 4 },
+        { id: "volcano", name: "Golden Volcano", namePl: "Złoty Wulkan", nameEn: "Golden Volcano", duration: 1800, baseDuration: 1800, baseCoins: 760, startCostCoins: 650, baseGems: 0, gemChance: 0.019, rareChance: 0.145, epicChance: 0.019, unlockLevel: 8 },
+        { id: "canyon", name: "Sunstone Canyon", namePl: "Kanion Słonecznego Kamienia", nameEn: "Sunstone Canyon", duration: 3600, baseDuration: 3600, baseCoins: 2100, startCostCoins: 1750, baseGems: 0, gemChance: 0.024, rareChance: 0.155, epicChance: 0.024, unlockLevel: 12 },
+        { id: "glacier", name: "Frozen Glacier", namePl: "Zamarznięty Lodowiec", nameEn: "Frozen Glacier", duration: 7200, baseDuration: 7200, baseCoins: 5600, startCostCoins: 4600, baseGems: 0, gemChance: 0.03, rareChance: 0.165, epicChance: 0.03, unlockLevel: 17 },
+        { id: "jungle", name: "Emerald Jungle", namePl: "Szmaragdowa Dżungla", nameEn: "Emerald Jungle", duration: 14400, baseDuration: 14400, baseCoins: 14500, startCostCoins: 11600, baseGems: 0, gemChance: 0.038, rareChance: 0.175, epicChance: 0.038, unlockLevel: 23 },
+        { id: "temple", name: "Ancient Temple", namePl: "Starożytna Świątynia", nameEn: "Ancient Temple", duration: 28800, baseDuration: 28800, baseCoins: 36000, startCostCoins: 28000, baseGems: 0, gemChance: 0.05, rareChance: 0.185, epicChance: 0.05, unlockLevel: 30 },
+        { id: "oasis", name: "Royal Oasis", namePl: "Królewska Oaza", nameEn: "Royal Oasis", duration: 43200, baseDuration: 43200, baseCoins: 80000, startCostCoins: 61000, baseGems: 0, gemChance: 0.062, rareChance: 0.195, epicChance: 0.062, unlockLevel: 38 },
+        { id: "kingdom", name: "Lost Beast Kingdom", namePl: "Zaginione Królestwo Bestii", nameEn: "Lost Beast Kingdom", duration: 86400, baseDuration: 86400, baseCoins: 180000, startCostCoins: 145000, baseGems: 0, gemChance: 0.078, rareChance: 0.21, epicChance: 0.078, unlockLevel: 48 }
     ],
 
     shopItems: [
@@ -49,8 +153,8 @@ CryptoZoo.config = {
             desc: "+1 coin do każdego kliknięcia",
             descPl: "+1 coin do każdego kliknięcia",
             descEn: "+1 coin to every click",
-            price: 1800,
-            priceScale: 1.24,
+            price: 2500,
+            priceScale: 1.30,
             type: "click",
             clickValueBonus: 1
         },
@@ -59,26 +163,26 @@ CryptoZoo.config = {
             name: "Strong Finger",
             namePl: "Silny Palec",
             nameEn: "Strong Finger",
-            desc: "+3 coins do każdego kliknięcia",
-            descPl: "+3 coins do każdego kliknięcia",
-            descEn: "+3 coins to every click",
-            price: 6500,
-            priceScale: 1.26,
+            desc: "+2 coins do każdego kliknięcia",
+            descPl: "+2 coins do każdego kliknięcia",
+            descEn: "+2 coins to every click",
+            price: 12000,
+            priceScale: 1.32,
             type: "click",
-            clickValueBonus: 3
+            clickValueBonus: 2
         },
         {
             id: "click3",
             name: "Turbo Tap",
             namePl: "Turbo Tap",
             nameEn: "Turbo Tap",
-            desc: "+5 coins do każdego kliknięcia",
-            descPl: "+5 coins do każdego kliknięcia",
-            descEn: "+5 coins to every click",
-            price: 18000,
-            priceScale: 1.28,
+            desc: "+4 coins do każdego kliknięcia",
+            descPl: "+4 coins do każdego kliknięcia",
+            descEn: "+4 coins to every click",
+            price: 48000,
+            priceScale: 1.35,
             type: "click",
-            clickValueBonus: 5
+            clickValueBonus: 4
         },
 
         {
@@ -89,8 +193,8 @@ CryptoZoo.config = {
             desc: "+1 level do wszystkich posiadanych zwierząt",
             descPl: "+1 level do wszystkich posiadanych zwierząt",
             descEn: "+1 level to all owned animals",
-            price: 18000,
-            priceScale: 1.27,
+            price: 95000,
+            priceScale: 1.40,
             type: "income",
             incomeBonus: 1
         },
@@ -99,26 +203,26 @@ CryptoZoo.config = {
             name: "Automation Crew",
             namePl: "Ekipa Automatyzacji",
             nameEn: "Automation Crew",
-            desc: "+2 level do wszystkich posiadanych zwierząt",
-            descPl: "+2 level do wszystkich posiadanych zwierząt",
-            descEn: "+2 levels to all owned animals",
-            price: 90000,
-            priceScale: 1.30,
+            desc: "+1 level do wszystkich posiadanych zwierząt",
+            descPl: "+1 level do wszystkich posiadanych zwierząt",
+            descEn: "+1 level to all owned animals",
+            price: 420000,
+            priceScale: 1.42,
             type: "income",
-            incomeBonus: 2
+            incomeBonus: 1
         },
         {
             id: "income3",
             name: "Elite Zoo Director",
             namePl: "Elitarny Dyrektor Zoo",
             nameEn: "Elite Zoo Director",
-            desc: "+4 level do wszystkich posiadanych zwierząt",
-            descPl: "+4 level do wszystkich posiadanych zwierząt",
-            descEn: "+4 levels to all owned animals",
-            price: 340000,
-            priceScale: 1.33,
+            desc: "+2 level do wszystkich posiadanych zwierząt",
+            descPl: "+2 level do wszystkich posiadanych zwierząt",
+            descEn: "+2 levels to all owned animals",
+            price: 2200000,
+            priceScale: 1.46,
             type: "income",
-            incomeBonus: 4
+            incomeBonus: 2
         },
 
         {
@@ -126,13 +230,13 @@ CryptoZoo.config = {
             name: "Daily Expedition Boost",
             namePl: "Lepszy Ekwipunek Ekspedycji",
             nameEn: "Daily Expedition Boost",
-            desc: "+25% reward z ekspedycji przez 24h",
-            descPl: "+25% reward z ekspedycji przez 24h",
-            descEn: "+25% expedition reward for 24h",
-            gemPrice: 2,
+            desc: "+20% reward z ekspedycji przez 24h",
+            descPl: "+20% reward z ekspedycji przez 24h",
+            descEn: "+20% expedition reward for 24h",
+            gemPrice: 3,
             priceScale: 1,
             type: "expedition",
-            expeditionBonus: 0.25
+            expeditionBonus: 0.20
         },
         {
             id: "expeditionTime1",
@@ -142,7 +246,7 @@ CryptoZoo.config = {
             desc: "Skraca czas jednej aktywnej ekspedycji o 10 minut",
             descPl: "Skraca czas jednej aktywnej ekspedycji o 10 minut",
             descEn: "Reduces one active expedition by 10 minutes",
-            gemPrice: 3,
+            gemPrice: 4,
             effect: "expeditionTime",
             timeReductionSeconds: 600
         },
@@ -154,7 +258,7 @@ CryptoZoo.config = {
             desc: "Skraca czas jednej aktywnej ekspedycji o 30 minut",
             descPl: "Skraca czas jednej aktywnej ekspedycji o 30 minut",
             descEn: "Reduces one active expedition by 30 minutes",
-            gemPrice: 7,
+            gemPrice: 8,
             effect: "expeditionTime",
             timeReductionSeconds: 1800
         },
@@ -166,7 +270,7 @@ CryptoZoo.config = {
             desc: "Skraca czas jednej aktywnej ekspedycji o 1 godzinę",
             descPl: "Skraca czas jednej aktywnej ekspedycji o 1 godzinę",
             descEn: "Reduces one active expedition by 1 hour",
-            gemPrice: 12,
+            gemPrice: 14,
             effect: "expeditionTime",
             timeReductionSeconds: 3600
         },
@@ -178,7 +282,7 @@ CryptoZoo.config = {
             desc: "Skraca czas jednej aktywnej ekspedycji o 4 godziny",
             descPl: "Skraca czas jednej aktywnej ekspedycji o 4 godziny",
             descEn: "Reduces one active expedition by 4 hours",
-            gemPrice: 40,
+            gemPrice: 48,
             effect: "expeditionTime",
             timeReductionSeconds: 14400
         }
