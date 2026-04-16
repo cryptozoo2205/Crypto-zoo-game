be097b90662017ebe67b4a5d2274d8ca1a229279
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-04-16 17:37:11 +0200

    Create animals-jungle-config.js

---
+++ backend/config/animals-jungle-config.js
@@ -0,0 +1,123 @@
+const ANIMALS_JUNGLE_CONFIG = {
+    monkey: {
+        name: "Monkey",
+        namePl: "Małpa",
+        nameEn: "Monkey",
+        asset: "monkey",
+        buyCost: 150,
+        baseIncome: 1,
+        unlockLevel: 1
+    },
+    panda: {
+        name: "Panda",
+        namePl: "Panda",
+        nameEn: "Panda",
+        asset: "panda",
+        buyCost: 850,
+        baseIncome: 4,
+        unlockLevel: 4
+    },
+    lion: {
+        name: "Lion",
+        namePl: "Lew",
+        nameEn: "Lion",
+        asset: "lion",
+        buyCost: 4500,
+        baseIncome: 11,
+        unlockLevel: 8
+    },
+    tiger: {
+        name: "Tiger",
+        namePl: "Tygrys",
+        nameEn: "Tiger",
+        asset: "tiger",
+        buyCost: 18000,
+        baseIncome: 26,
+        unlockLevel: 12
+    },
+    elephant: {
+        name: "Elephant",
+        namePl: "Słoń",
+        nameEn: "Elephant",
+        asset: "elephant",
+        buyCost: 78000,
+        baseIncome: 58,
+        unlockLevel: 16
+    },
+    giraffe: {
+        name: "Giraffe",
+        namePl: "Żyrafa",
+        nameEn: "Giraffe",
+        asset: "giraffe",
+        buyCost: 340000,
+        baseIncome: 125,
+        unlockLevel: 20
+    },
+    zebra: {
+        name: "Zebra",
+        namePl: "Zebra",
+        nameEn: "Zebra",
+        asset: "zebra",
+        buyCost: 1450000,
+        baseIncome: 265,
+        unlockLevel: 24
+    },
+    hippo: {
+        name: "Hippo",
+        namePl: "Hipopotam",
+        nameEn: "Hippo",
+        asset: "hippo",
+        buyCost: 6200000,
+        baseIncome: 560,
+        unlockLevel: 28
+    },
+    penguin: {
+        name: "Penguin",
+        namePl: "Pingwin",
+        nameEn: "Penguin",
+        asset: "penguin",
+        buyCost: 27000000,
+        baseIncome: 1180,
+        unlockLevel: 32
+    },
+    bear: {
+        name: "Bear",
+        namePl: "Niedźwiedź",
+        nameEn: "Bear",
+        asset: "bear",
+        buyCost: 120000000,
+        baseIncome: 2450,
+        unlockLevel: 36
+    },
+    crocodile: {
+        name: "Crocodile",
+        namePl: "Krokodyl",
+        nameEn: "Crocodile",
+        asset: "crocodile",
+        buyCost: 540000000,
+        baseIncome: 5100,
+        unlockLevel: 40
+    },
+    kangaroo: {
+        name: "Kangaroo",
+        namePl: "Kangur",
+        nameEn: "Kangaroo",
+        asset: "kangaroo",
+        buyCost: 2400000000,
+        baseIncome: 10400,
+        unlockLevel: 44
+    },
+    wolf: {
+        name: "Wolf",
+        namePl: "Wilk",
+        nameEn: "Wolf",
+        asset: "wolf",
+        buyCost: 10500000000,
+        baseIncome: 21000,
+        unlockLevel: 48
+    }
+};
+
+module.exports = {
+    ANIMALS_JUNGLE_CONFIG
+};
