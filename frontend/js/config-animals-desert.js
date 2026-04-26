573e4f992d2807e8c612eb219730d30fca415d90
cryptozoo2205 <harasimowiczmichal5@gmail.com>
2026-04-26 20:31:33 +0200

    Update config-animals-desert.js

--- frontend/js/config-animals-desert.js
+++ frontend/js/config-animals-desert.js
@@ -1,1 +1,85 @@
-tet
+window.CryptoZoo = window.CryptoZoo || {};
+CryptoZoo.config = CryptoZoo.config || {};
+CryptoZoo.config.animals = CryptoZoo.config.animals || {};
+
+Object.assign(CryptoZoo.config.animals, {
+    camel: {
+        name: "Camel",
+        namePl: "Wielbłąd",
+        nameEn: "Camel",
+        asset: "camel",
+        buyCost: 12000000000,
+        baseIncome: 6200,
+        unlockLevel: 50
+    },
+
+    fennec: {
+        name: "Fennec",
+        namePl: "Fenek",
+        nameEn: "Fennec",
+        asset: "fennec",
+        buyCost: 22000000000,
+        baseIncome: 8200,
+        unlockLevel: 55
+    },
+
+    scorpion: {
+        name: "Scorpion",
+        namePl: "Skorpion",
+        nameEn: "Scorpion",
+        asset: "scorpion",
+        buyCost: 42000000000,
+        baseIncome: 11000,
+        unlockLevel: 60
+    },
+
+    cobra: {
+        name: "Cobra",
+        namePl: "Kobra",
+        nameEn: "Cobra",
+        asset: "cobra",
+        buyCost: 78000000000,
+        baseIncome: 15000,
+        unlockLevel: 65
+    },
+
+    meerkat: {
+        name: "Meerkat",
+        namePl: "Surykatka",
+        nameEn: "Meerkat",
+        asset: "meerkat",
+        buyCost: 145000000000,
+        baseIncome: 20500,
+        unlockLevel: 70
+    },
+
+    vulture: {
+        name: "Vulture",
+        namePl: "Sęp",
+        nameEn: "Vulture",
+        asset: "vulture",
+        buyCost: 260000000000,
+        baseIncome: 28500,
+        unlockLevel: 76
+    },
+
+    hyena: {
+        name: "Hyena",
+        namePl: "Hiena",
+        nameEn: "Hyena",
+        asset: "hyena",
+        buyCost: 480000000000,
+        baseIncome: 39500,
+        unlockLevel: 83
+    },
+
+    anubis: {
+        name: "Anubis",
+        namePl: "Anubis",
+        nameEn: "Anubis",
+        asset: "anubis",
+        buyCost: 900000000000,
+        baseIncome: 55000,
+        unlockLevel: 90
+    }
+});
